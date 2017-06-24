8a067cf12f13a44c6c73aa30cfa77b2f9c4396ad
Mikhail Metrikin <metrikin@gmail.com>
2017-06-23 17:50:24 -0700

    Model and admin started

--- iSnippets/snippets/models.py
+++ iSnippets/snippets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Snippet(models.Model):
+    title = models.CharField(max_length=256, blank=False, null=False)
+    language = models.CharField(max_length=256, blank=False, null=False)
+    snippet = models.TextField(blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return "Title: %s\nLanguage:%s\nSnippet:%s\nDescription:%s\n" %(self.title, self.language, self.snippet, self.description)
+    
